2308364552ee5ba5da8f206e799595dfb14c9dd5
Axel-PC <empdreichels@gmail.com>
2018-02-18 22:21:18 +0300

    Readd Register button functionality in Login Page

--- app/src/main/java/com/c50x/eleos/activities/LoginActivity.java
+++ app/src/main/java/com/c50x/eleos/activities/LoginActivity.java
@@ -1,41 +1,14 @@
 package com.c50x.eleos.activities;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.c50x.eleos.R;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
@@ -62,7 +35,13 @@ public class LoginActivity extends AppCompatActivity
         register_button = (Button) findViewById(R.id.Register_button);
         mLoginFormView = findViewById(R.id.login_form);
 
-        
+        register_button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
