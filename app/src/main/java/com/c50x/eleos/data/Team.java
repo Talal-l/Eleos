e7fd0c18296a53a6f3d83d026838321febbf7b5c
talal <talal__l@hotmail.com>
2018-02-28 16:49:26 +0300

    Create team entity
    A table for teams in the SQL database

--- app/src/main/java/com/c50x/eleos/data/Team.java
+++ app/src/main/java/com/c50x/eleos/data/Team.java
@@ -1,8 +1,40 @@
 package com.c50x.eleos.data;
 
-/**
- * Created by ugin on 2/16/18.
- */
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
 
+@Entity
 public class Team {
+
+    @PrimaryKey
+    @NonNull
+    private String teamName;
+    private String sport;
+    private String teamAdmin;
+
+    @NonNull
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(@NonNull String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public String getTeamAdmin() {
+        return teamAdmin;
+    }
+
+    public void setTeamAdmin(String teamAdmin) {
+        this.teamAdmin = teamAdmin;
+    }
 }
