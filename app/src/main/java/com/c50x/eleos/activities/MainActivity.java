90663b0c321126b58a2d224d0c382ed94307562d
joegriffin95 <yousefhusain0295@gmail.com>
2018-02-28 16:04:30 +0300

    changed design of navigation menu and added action bar

--- app/src/main/java/com/c50x/eleos/activities/MainActivity.java
+++ app/src/main/java/com/c50x/eleos/activities/MainActivity.java
@@ -2,8 +2,11 @@ package com.c50x.eleos.activities;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -22,7 +25,8 @@ public class MainActivity extends AppCompatActivity
     private TextView nameView;
     private TextView emailView;
     private User[] l;
-
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     // Create an AsyncTask class so the database operations can be done in the background
     private class DatabaseAsync extends AsyncTask<Void,Void,Void> {
@@ -53,13 +57,12 @@ public class MainActivity extends AppCompatActivity
                 Log.w("email: ", l[i].getEmail());
             }
 
-            emailView.append(currentUser.getEmail());
-            nameView.append(currentUser.getName());
+            //emailView.append(currentUser.getEmail());
+            //nameView.append(currentUser.getName());
             handleView.append(currentUser.getHandle());
             return null;
         }
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -72,14 +75,24 @@ public class MainActivity extends AppCompatActivity
         new DatabaseAsync().execute();
 
         handleView = findViewById(R.id.activity_main_user_handle_textView);
-        nameView = findViewById(R.id.activity_main_user_name_textView);
-        emailView = findViewById(R.id.user_email_textView);
 
+        // for navigation menu
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open , R.string.close);
 
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
 
+        if(getSupportActionBar() != null)
+             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(mToggle.onOptionsItemSelected(item))
+            return true;
 
-
-
+        return super.onOptionsItemSelected(item);
     }
 }
