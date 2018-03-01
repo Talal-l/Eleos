894147ffb6ea4869e22f05678d7524f7821373cd
talal <talal__l@hotmail.com>
2018-03-02 00:00:42 +0300

    Refactor loginActivity and move database code to LoginTask

--- app/src/main/java/com/c50x/eleos/activities/LoginActivity.java
+++ app/src/main/java/com/c50x/eleos/activities/LoginActivity.java
@@ -13,6 +13,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.c50x.eleos.R;
+import com.c50x.eleos.controllers.LoginTask;
 import com.c50x.eleos.data.AppDatabase;
 import com.c50x.eleos.data.User;
 
@@ -30,76 +31,56 @@ public class LoginActivity extends AppCompatActivity
     private View loginFormView;
     private Button sign_in_button;
     private Button register_button;
-    private AppDatabase db;
-    private User l[];
+    private String userEmail;
+    private String userPassword;
 
 
 
-
-    // Create an AsyncTask class so the database operations can be done in the background
-    private class DatabaseAsync extends AsyncTask<Void,Void,Void> {
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-
-                sign_in_button.setOnClickListener(new View.OnClickListener() // handles sign in button click
-                {
-                    @Override
-                    public void onClick(View v) // checks if user input was correct by verifying email and password
-                    {
-                        Log.w("eamil: ", emailView.getText().toString());
-
-                        l = db.userDao().loadUserWithEmail(emailView.getText().toString());
-                        if (!validateEmail(emailView.getText().toString())) // if not a valid email address
-                        {
-                            emailView.setError("Invalid Email");
-                            emailView.requestFocus();
-                        }
-                        else if (!validatePassword(passwordView.getText().toString()))// if not a valid password
-                        {
-                            passwordView.setError("Invalid Password");
-                            passwordView.requestFocus();
-                        }
-                        else if (l.length == 0){ // Email not in database
-                            emailView.setError("No account with this email");
-                            emailView.requestFocus();
-                        }
-                        else if (l.length > 0 && !l[0].getPassword().equals(passwordView.getText().toString())){
-                            emailView.setError("Incorrect password");
-                            emailView.requestFocus();
-                        }
-                        else // valid email and password
-                        {
-                            Toast.makeText(LoginActivity.this, "LogIn successful !", Toast.LENGTH_LONG).show(); // prints on the screen that log in was successful
-                            Intent intent2 = new Intent(v.getContext(), MainActivity.class);
-                            intent2.putExtra("from","login"); // To tell what screen we came from
-                            // To load the logged in player when they move to the main screen
-                            intent2.putExtra("email",emailView.getText().toString());
-                            startActivity(intent2); // switch to main activity
-                        }
-                    }
-                });
-
-            return null;
-
-
-            }
-        }
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN); //hides keyboard upon switching to this Activity
         setContentView(R.layout.activity_login);
-        db = AppDatabase.getDatabaseInstance(getApplicationContext());
-        new DatabaseAsync().execute();
-
 
         // Set up the login form.
-        emailView = (AutoCompleteTextView) findViewById(R.id.email);
-        passwordView = (EditText) findViewById(R.id.password);
-        sign_in_button = (Button) findViewById(R.id.signIn_button);
-        register_button = (Button) findViewById(R.id.Register_button);
+        emailView = findViewById(R.id.email);
+        passwordView = findViewById(R.id.password);
+        sign_in_button = findViewById(R.id.signIn_button);
+        register_button = findViewById(R.id.Register_button);
+
+        sign_in_button.setOnClickListener(new View.OnClickListener() // handles sign in button click
+        {
+            @Override
+            public void onClick(View v) // checks if user input was correct by verifying email and password
+            {
+                userEmail = emailView.getText().toString();
+                userPassword = passwordView.getText().toString();
+                Log.w("eamil: ", userEmail);
+
+                if (!validateEmail(userEmail)) // if not a valid email address
+                {
+                    emailView.setError("Invalid Email");
+                    emailView.requestFocus();
+                }
+                else if (!validatePassword(userPassword))// if not a valid password
+                {
+                    passwordView.setError("Invalid Password");
+                    passwordView.requestFocus();
+                }
+
+                else{ // syntax is correct for password and email
+
+                    // Check if user has an account (in database)
+
+                    // pass the activityContext so we can change UI elements from LoginTask
+                    LoginTask login = new LoginTask(getApplicationContext(),LoginActivity.this);
+                    login.authUsingEmail(userEmail,userPassword);
+
+
+                }
+            }
+        });
 
 
         register_button.setOnClickListener(new View.OnClickListener() // handles register button click
--- app/src/main/java/com/c50x/eleos/controllers/LoginTask.java
+++ app/src/main/java/com/c50x/eleos/controllers/LoginTask.java
@@ -1,48 +1,70 @@
 package com.c50x.eleos.controllers;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
+import com.c50x.eleos.R;
+import com.c50x.eleos.activities.MainActivity;
 import com.c50x.eleos.data.AppDatabase;
 import com.c50x.eleos.data.User;
 
 public class LoginTask {
     private AppDatabase db;
-    private User userToAuth;
-    private int authState;
+    private Context context;
     private User userList[];
 
+    public LoginTask(Context appContext, Context activityContext){
+        this.context = activityContext;
+        db = AppDatabase.getDatabaseInstance(appContext);
+    }
 
-    private class AuthUsingEmail extends AsyncTask<Void,Void,Void> {
-        AuthUsingEmail(Context context, User user) {
-            db = AppDatabase.getDatabaseInstance(context);
-            userToAuth = user;
-        }
+    private class AuthUsingEmail extends AsyncTask<String,Void,Boolean> {
 
         @Override
-        protected Void doInBackground(Void... voids) {
+        protected Boolean doInBackground(String ... par) {
+
+            String userEmail = par[0];
+            String userPassword = par[1];
 
             // TODO check with remote server instead of local
 
             // Check if the user email and password match with the database
-            userList = db.userDao().authUsingEmail(userToAuth.getEmail(), userToAuth.getPassword());
-            return null;
+            userList = db.userDao().authUsingEmail(userEmail, userPassword);
+            if (userList.length == 1)
+                return true;
+
+        return false;
+    }
+
+        protected void onPostExecute(Boolean success){
+
+            AutoCompleteTextView emailView = ((Activity) context).findViewById(R.id.email);
+            if (success) {
+                Toast.makeText(context, "LogIn successful !", Toast.LENGTH_LONG).show(); // prints on the screen that log in was successful
+                Intent intent = new Intent(context, MainActivity.class);
+                intent.putExtra("from", "login"); // To tell what screen we came from
+
+                // To load the logged in player when they move to the main screen
+                intent.putExtra("email", emailView.getText().toString());
+                context.startActivity(intent); // switch to main activity
+            }
+            else {
+                // Show error message to user
+                // TODO better message
+
+                emailView.setError("Incorrect Email or Password");
+                emailView.requestFocus();
+            }
         }
     }
-        private class AuthUsingHandle extends AsyncTask<Void,Void,Void> {
-        AuthUsingHandle(Context context, User user) {
-            db = AppDatabase.getDatabaseInstance(context);
-            userToAuth = user;
-        }
 
-        @Override
-        protected Void doInBackground(Void... voids) {
-
-            // TODO check with remote server instead of local
-
-            // Check if the user email and password match with the database
-            userList = db.userDao().authUsingHandle(userToAuth.getHandle(), userToAuth.getPassword());
-            return null;
-        }
+   public void authUsingEmail(String email, String password){
+        new AuthUsingEmail().execute(email,password);
     }
+
+
 }
