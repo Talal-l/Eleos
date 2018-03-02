0c7f777abaaaade9266d42dcba74b918c3a4d84d
talal_mac <talal__l@hotmail.com>
2018-03-02 20:58:00 +0300

    Add network code to player search activity

--- app/src/main/java/com/c50x/eleos/activities/PlayerSearchActivity.java
+++ app/src/main/java/com/c50x/eleos/activities/PlayerSearchActivity.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.c50x.eleos.R;
+import com.c50x.eleos.controllers.UserTask;
 
 public class PlayerSearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playersearch);
+
+
+        UserTask search = new UserTask(getApplicationContext(),PlayerSearchActivity.this);
+        search.loadAllPlayers();
 
     }
 
--- app/src/main/java/com/c50x/eleos/controllers/UserTask.java
+++ app/src/main/java/com/c50x/eleos/controllers/UserTask.java
@@ -2,9 +2,19 @@ package com.c50x.eleos.controllers;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.c50x.eleos.data.AppDatabase;
 import com.c50x.eleos.data.User;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class UserTask {
     private AppDatabase db;
@@ -17,16 +27,63 @@ public class UserTask {
         db = AppDatabase.getDatabaseInstance(appContext);
     }
 
-    private class LoadAllUsers extends AsyncTask<Void,Void,Void> {
+    private class LoadAllUsers extends AsyncTask<String,String,JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(String ... par) {
+
+
+            String str="http://192.168.43.171:8888/android_connect/get_all_players.php";
+            URLConnection urlConn = null;
+            BufferedReader bufferedReader = null;
+            try
+            {
+
+                URL url = new URL(str);
+                urlConn = url.openConnection();
+                bufferedReader = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+
+                StringBuffer stringBuffer = new StringBuffer();
+                String line;
+                while ((line = bufferedReader.readLine()) != null)
+                {
+                    stringBuffer.append(line);
+                }
+                return new JSONObject(stringBuffer.toString());
+
+
+            }
+            catch(Exception ex)
+            {
+                Log.e("App", "yourDataTask", ex);
+                return null;
+            }
+            finally
+            {
+                if(bufferedReader != null)
+                {
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
 
 
         @Override
-        protected Void doInBackground(Void... voids) {
-            return null;
-        }
-
-        protected void onPostExecute(Long rowNum){
-
+        protected void onPostExecute(JSONObject response)
+        {
+            if(response != null)
+            {
+                try {
+                    Log.e("App", "Success: " + response.getString("yourJsonElement") );
+                } catch (JSONException ex) {
+                    Log.e("App", "Failure", ex);
+                }
+            }
         }
     }
     public void loadAllPlayers(){
