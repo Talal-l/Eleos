4c3cc869dbb4719c964dd6cf1913b3b884f49bd7
talal_mac <talal__l@hotmail.com>
2018-03-02 19:59:01 +0300

    Add UserTasks to handle database access for user

--- app/src/main/java/com/c50x/eleos/controllers/UserTask.java
+++ app/src/main/java/com/c50x/eleos/controllers/UserTask.java
@@ -1,5 +1,37 @@
 package com.c50x.eleos.controllers;
 
-public class PlayerTask {
+import android.content.Context;
+import android.os.AsyncTask;
 
+import com.c50x.eleos.data.AppDatabase;
+import com.c50x.eleos.data.User;
+
+public class UserTask {
+    private AppDatabase db;
+    private Context context;
+    private User userList[];
+
+
+    public UserTask(Context appContext, Context activityContext){
+        this.context = activityContext;
+        db = AppDatabase.getDatabaseInstance(appContext);
+    }
+
+    private class LoadAllUsers extends AsyncTask<Void,Void,Void> {
+
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
+
+        protected void onPostExecute(Long rowNum){
+
+        }
+    }
+    public void loadAllPlayers(){
+
+        new LoadAllUsers().execute();
+
+    }
 }
