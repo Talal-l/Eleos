b9b447df78f7001b1f0c67fe98e035432a729163
talal <talal__l@hotmail.com>
2018-03-03 18:08:32 +0300

    Add method to load team from remote database

--- app/src/main/java/com/c50x/eleos/controllers/TeamTask.java
+++ app/src/main/java/com/c50x/eleos/controllers/TeamTask.java
@@ -12,6 +12,7 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
+import java.util.AbstractMap;
 import java.util.Map;
 
 import okhttp3.MediaType;
@@ -27,6 +28,7 @@ public class TeamTask{
     private String urlBase;
     private OkHttpClient client;
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private Gson gson;
 
     public TeamTask(Context appContext, Context activityContext){
         this.context = activityContext;
@@ -35,6 +37,8 @@ public class TeamTask{
         urlBase = appContext.getString(R.string.server_address);
         // create the client used to make the requests
         client = new OkHttpClient();
+
+        gson = new Gson();
     }
 
 
@@ -60,22 +64,23 @@ public class TeamTask{
 
 
 
+    // add team to remote database
 
-    private class addTeam extends AsyncTask<Team,Void,String> {
+    private class AddTeam extends AsyncTask<Team,Void,String> {
 
         @Override
         protected String doInBackground(Team... par) {
             Team teamToAdd = par[0];
+            String script = "/addTeam.php";
 
             // convert into a JSON object
-            Gson gson = new Gson();
             String json = gson.toJson(teamToAdd);
-            Log.i("TeamTask", "json to be sent: " + json);
+            Log.i("TeamTask_addTeam", "json to be sent: " + json);
 
             // construct the url
-            Log.i("TeamTask", "using server address: " + urlBase);
-            String url = urlBase + "/addTeam.php";
-            Log.i("TeamTask", "url: " + url);
+            String url = urlBase + script;
+            Log.i("TeamTask_addTeam", "using server address: " + urlBase);
+            Log.i("TeamTask_addTeam", "url: " + url);
 
             // send post request
             String response;
@@ -91,28 +96,93 @@ public class TeamTask{
 
         protected void onPostExecute(String response){
 
-            Gson gson = new Gson();
             // convert json to Map
             Map<String,Object> responseMap = gson.fromJson(response,
                     new TypeToken<Map<String,Object>>(){}.getType());
 
             // print responseMap to logCat
-            Log.i("TeamTask", "response hashMap ");
+            Log.i("TeamTask_addTeam", "response hashMap ");
             for (Map.Entry<String,Object> entry: responseMap.entrySet()){
                 String key = entry.getKey();
                 Object value = entry.getValue();
-                Log.i("TeamTask","key: " + key + "value: " + value.toString());
+                Log.i("TeamTask_addTeam","key: " + key + "value: " + value.toString());
+            }
+        }
+    }
+    public void addTeam(Team team){
+
+        new AddTeam().execute(team);
+
+    }
+
+
+    // load team from remote database
+
+    private class LoadTeam extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected String doInBackground(String ... par) {
+            String teamName = par[0];
+            String key = "teamName"; // TODO: Match with column name in server
+            String script = "/loadTeam.php";
+
+
+            // create a key value pair
+
+            AbstractMap.SimpleEntry<String,String> keyValue =
+                    new AbstractMap.SimpleEntry<>(key, teamName);
+
+
+            // convert into a JSON object
+
+            String json = gson.toJson(keyValue);
+            Log.i("TeamTask_loadTeam", "json to be sent: " + json);
+
+
+            // construct the url
+
+            String url = urlBase + script;
+            Log.i("TeamTask_loadTeam", "using server address: " + urlBase);
+            Log.i("TeamTask_loadTeam", "url: " + url);
+
+
+            // send post request
+
+            String response;
+            try {
+
+                response = postRequest(url,json);
+            }catch (Exception e){
+                response = "connection broken";
+            }
+
+        return response;
+    }
+
+        protected void onPostExecute(String response){
+
+            // convert json to Map
+            Map<String,Object> responseMap = gson.fromJson(response,
+                    new TypeToken<Map<String,Object>>(){}.getType());
+
+            // print responseMap to logCat
+            Log.i("TeamTask_loadTeam", "response hashMap ");
+            for (Map.Entry<String,Object> entry: responseMap.entrySet()){
+                String key = entry.getKey();
+                Object value = entry.getValue();
+                Log.i("TeamTask_loadTeam","key: " + key + "value: " + value.toString());
             }
         }
     }
 
+ public void loadTeam (String teamName){
 
-    public void addTeam(Team team){
-
-        new addTeam().execute(team);
-
+        new LoadTeam().execute(teamName);
     }
 
 
-    // get team
+
+
+
+
 }
