10bd9e1d9a97f01aa7f270dc8a85c6b3d87fef3c
talal <talal__l@hotmail.com>
2018-03-03 17:35:51 +0300

    Add method to add team to remote database

--- app/src/main/java/com/c50x/eleos/controllers/TeamTask.java
+++ app/src/main/java/com/c50x/eleos/controllers/TeamTask.java
@@ -2,57 +2,117 @@ package com.c50x.eleos.controllers;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.c50x.eleos.R;
 import com.c50x.eleos.data.AppDatabase;
 import com.c50x.eleos.data.Team;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.util.Map;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class TeamTask{
     private AppDatabase db;
     private Context context;
     private Team teamList[];
-
+    private String urlBase;
+    private OkHttpClient client;
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     public TeamTask(Context appContext, Context activityContext){
         this.context = activityContext;
         db = AppDatabase.getDatabaseInstance(appContext);
+        // load the server address from string.xml
+        urlBase = appContext.getString(R.string.server_address);
+        // create the client used to make the requests
+        client = new OkHttpClient();
     }
 
-    private class addTeam extends AsyncTask<Team,Void,Long> {
+
+    // common methods
+    private String getRequest(String url) throws IOException{
+        Request request = new Request.Builder().url(url).build();
+
+        Response response = client.newCall(request).execute();
+
+        return response.body().toString();
+    }
+
+    private String postRequest(String url, String json) throws IOException {
+
+        RequestBody body = RequestBody.create(JSON,json);
+        Request request = new Request.Builder().url(url).post(body).build();
+
+        Response response = client.newCall(request).execute();
+
+        return response.body().toString();
+    }
+
+
+
+
+
+    private class addTeam extends AsyncTask<Team,Void,String> {
 
         @Override
-        protected Long doInBackground(Team... par) {
-
+        protected String doInBackground(Team... par) {
             Team teamToAdd = par[0];
 
+            // convert into a JSON object
+            Gson gson = new Gson();
+            String json = gson.toJson(teamToAdd);
+            Log.i("TeamTask", "json to be sent: " + json);
 
-            // Check if the user email and password match with the database
-            Long state;
+            // construct the url
+            Log.i("TeamTask", "using server address: " + urlBase);
+            String url = urlBase + "/addTeam.php";
+            Log.i("TeamTask", "url: " + url);
 
-            // TODO: What is the return on abort?
-            state = db.teamDao().addTeam(teamToAdd);
+            // send post request
+            String response;
+            try {
 
-        return state;
+                response = postRequest(url,json);
+            }catch (Exception e){
+                response = "connection broken";
+            }
+
+        return response;
     }
 
-        protected void onPostExecute(Long rowNum){
+        protected void onPostExecute(String response){
 
-            if (rowNum >= 0) { // TODO: Check if this is correct
-                Toast.makeText(context, "Team created successfully !", Toast.LENGTH_LONG).show(); // prints on the screen that log in was successful
+            Gson gson = new Gson();
+            // convert json to Map
+            Map<String,Object> responseMap = gson.fromJson(response,
+                    new TypeToken<Map<String,Object>>(){}.getType());
 
-                // TODO: Show created team Info without the fields being editable
-            }
-            else {
-                // TODO: Show error that name exist
-                // load view from activity context and display message
-
+            // print responseMap to logCat
+            Log.i("TeamTask", "response hashMap ");
+            for (Map.Entry<String,Object> entry: responseMap.entrySet()){
+                String key = entry.getKey();
+                Object value = entry.getValue();
+                Log.i("TeamTask","key: " + key + "value: " + value.toString());
             }
         }
     }
+
 
     public void addTeam(Team team){
 
         new addTeam().execute(team);
 
     }
+
+
+    // get team
 }
