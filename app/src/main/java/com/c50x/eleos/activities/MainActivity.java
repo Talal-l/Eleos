c8dafb2acece8b626dec5e5d8fb7164ae3e81c8d
Axel-PC <empdreichels@gmail.com>
2018-03-04 23:47:21 +0300

    Fix activity_create_team design

--- app/src/main/java/com/c50x/eleos/activities/MainActivity.java
+++ app/src/main/java/com/c50x/eleos/activities/MainActivity.java
@@ -4,41 +4,19 @@ import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.sax.StartElementListener;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import com.c50x.eleos.R;
 import com.c50x.eleos.data.AppDatabase;
 import com.c50x.eleos.data.User;
-import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
-import android.support.v4.view.GravityCompat;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.view.LayoutInflater;
-import android.view.MenuInflater;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-import android.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
-import android.view.Gravity;
-import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -131,6 +109,11 @@ public class MainActivity extends AppCompatActivity
                                 break;
                             case R.id.nav_menu_teamCreation:
                                 intent = new Intent(MainActivity.this, CreateTeamActivity.class);
+                                startActivity(intent);
+                                break;
+
+                            case R.id.nav_menu_playerSearch:
+                                intent = new Intent(MainActivity.this, PlayerSearchActivity.class);
                                 startActivity(intent);
                                 break;
                         }
--- app/src/main/java/com/c50x/eleos/activities/PlayerSearchActivity.java
+++ app/src/main/java/com/c50x/eleos/activities/PlayerSearchActivity.java
@@ -1,20 +1,30 @@
 package com.c50x.eleos.activities;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.c50x.eleos.R;
-import com.c50x.eleos.controllers.AsyncResponse;
 import com.c50x.eleos.controllers.TeamTask;
 import com.c50x.eleos.data.Team;
 
-public class PlayerSearchActivity extends AppCompatActivity implements AsyncResponse {
+public class PlayerSearchActivity extends AppCompatActivity {
 
     private TeamTask task;
     private Team newTeam;
 
+    private Context activityContext;
+
+    private EditText playerSearch;
+    private Button confirmButton;
+    private Button cancelButton;
+
+    int flag = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +35,45 @@ public class PlayerSearchActivity extends AppCompatActivity implements AsyncResp
 
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN); //hides keyboard upon switching to this Activity
 
+        playerSearch = (EditText) findViewById(R.id.player_search);
+        confirmButton = (Button) findViewById(R.id.button_confirm);
+        cancelButton = (Button) findViewById(R.id.button_cancel);
 
 
+        confirmButton.setOnClickListener(new View.OnClickListener() {
 
-    }
+            String[] androidStrings = getResources().getStringArray(R.array.dummy1);
 
+            @Override
+            public void onClick(View v) {
 
-    @Override
-    public void taskFinished(String output) {
+                if (playerSearch.getText().toString().equals("")) {
+                    playerSearch.setError("Empty");
+                } else {
+                    for (int i = 0; i < androidStrings.length; i++) {
+                        if (playerSearch.getText().toString().equals((androidStrings[i]))) {
+                            flag = 1;
+                        }
+                        else
+                            continue;
+                    }
+                    if (flag == 0)
+                        playerSearch.setError("Player Does Not Exist");
+
+                    else if (flag == 1)
+                        playerSearch.setError("Player Exists");
+                }
+            }
+        });
+
+        /*cancelButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PlayerSearchActivity.this, MainActivity.class);
+                activityContext.startActivity(intent);
+            }
+        });*/
 
     }
 }
