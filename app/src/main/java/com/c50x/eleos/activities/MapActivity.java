6c601d6738baaccd7f9aee60e47b1e8ac9ac1e00
talal <talal__l@hotmail.com>
2018-04-15 01:07:30 +0300

    Zoom to venue location on map and place a marker

--- app/src/main/java/com/c50x/eleos/activities/MapActivity.java
+++ app/src/main/java/com/c50x/eleos/activities/MapActivity.java
@@ -2,111 +2,85 @@ package com.c50x.eleos.activities;
 
 
 import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
 import com.c50x.eleos.R;
 import com.c50x.eleos.data.Venue;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
-{
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
     private static final String TAG = "MapActivity";
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
-    private Boolean mLocationPermissionGranted = false;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private Boolean mLocationPermissionGranted = false;
     private GoogleMap mMap;
     private Venue gameVenue;
     private Gson gson;
 
     @Override
-    public void onMapReady(GoogleMap googleMap)
-    {
+    public void onMapReady(GoogleMap googleMap) {
         Toast.makeText(this, "Map is ready", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onMapReady: map is ready");
         mMap = googleMap;
+        String[] coordinate = gameVenue.getVenueCoordinate().split(",");
+
+        double la  = Double.parseDouble(coordinate[0]);
+        double lng = Double.parseDouble(coordinate[1]);
+
+        // zoom to location and place a marker
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(la, lng), 16));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(la,lng)).title(gameVenue.getVenueName()));
+
+
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_maps);
-
-        getLocationPermission();
 
         gson = new Gson();
 
         // get selected venue object
         String gameVenueJson = getIntent().getStringExtra("gameVenue");
         Log.i(TAG, "onCreate gameVenueJson: " + gameVenueJson);
-        gameVenue = gson.fromJson(gameVenueJson,Venue.class);
-    }
+        gameVenue = gson.fromJson(gameVenueJson, Venue.class);
 
-    private void initMap()
-    {
+
         Log.d(TAG, "initMap: initializing map");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(MapActivity.this);
-    }
 
-    private void getLocationPermission()
-    {
-        Log.d(TAG, "getLocationPermission: getting loaction permissions");
-        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
-
-        if(ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-        {
-            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-                mLocationPermissionGranted = true;
-
-            else
-                ActivityCompat.requestPermissions(this, permissions,LOCATION_PERMISSION_REQUEST_CODE);
-        }
-        else
-            ActivityCompat.requestPermissions(this, permissions,LOCATION_PERMISSION_REQUEST_CODE);
 
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-    {
-        Log.d(TAG, "onRequestPermissionResult: called");
-        mLocationPermissionGranted = false;
 
-        switch(requestCode)
-        {
-            case LOCATION_PERMISSION_REQUEST_CODE:
-            {
-                if(grantResults.length > 0)
-                {
-                    for(int i = 0 ; i < grantResults.length ; i++)
-                    {
-                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED)
-                        {
-                            mLocationPermissionGranted = false;
-                            Log.d(TAG, "onRequestPermissionResult: permission failed");
-                            return;
-                        }
-                    }
-                    Log.d(TAG, "onRequestPermissionResult: permission granted");
-                    mLocationPermissionGranted = true;
-                    initMap();
-                }
-            }
+    private boolean checkReady() {
+        if (mMap == null) {
+            Toast.makeText(this, "Map not ready yet", Toast.LENGTH_SHORT).show();
+            return false;
         }
+        return true;
+    }
+
+    public void onGoToBondi(View view) {
+        if (!checkReady()) {
+            return;
+        }
+
     }
 
 
